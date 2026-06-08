27bb5efa88749eff13d9ff70cf245e8ef9b33b7f
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-06-08 13:23:13 +0200

    add cloud block to infisical

--- terraform/services/infisical/terraform.tf
+++ terraform/services/infisical/terraform.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = "~> 1.15"
 
+  cloud {
+    organization = "zemn"
+
+    workspaces {
+      name = "infisical"
+    }
+  }
+
   required_providers {
     infisical = {
       source  = "infisical/infisical"

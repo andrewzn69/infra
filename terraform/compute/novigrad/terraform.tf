14e3a877062bbc95799194392a20430378fc5f05
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-06-08 23:24:15 +0200

    set oci version to ~> 8.14

--- terraform/compute/novigrad/terraform.tf
+++ terraform/compute/novigrad/terraform.tf
@@ -20,7 +20,7 @@ terraform {
     }
     oci = {
       source  = "oracle/oci"
-      version = "~> 8.14.0"
+      version = "~> 8.14"
     }
     tailscale = {
       source  = "tailscale/tailscale"

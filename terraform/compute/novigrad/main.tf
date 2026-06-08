3863f44c1c2faa185b1f5717bd05f4032756a055
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-06-08 22:29:12 +0200

    bump tf-oci-free-oke module version to v0.1.3

--- terraform/compute/novigrad/main.tf
+++ terraform/compute/novigrad/main.tf
@@ -8,7 +8,7 @@ resource "tailscale_tailnet_key" "node" {
 }
 
 module "oke" {
-  source = "git::https://github.com/andrewzn69/tf-oci-free-oke.git?ref=v0.1.2"
+  source = "git::https://github.com/andrewzn69/tf-oci-free-oke.git?ref=v0.1.3"
 
   compartment_id           = var.compartment_id
   name                     = var.name

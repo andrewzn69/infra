7310c8e1f56ffa540581a3561a46832fa3e8aeaf
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-06-10 21:03:17 +0200

    fix blkid syntax and templatefile escaping

--- terraform/compute/novigrad/cloud-init.sh
+++ terraform/compute/novigrad/cloud-init.sh
@@ -14,13 +14,13 @@ fi
 /usr/libexec/oci-growfs -y
 
 # format and mount data volume
-if ! blkid /dev/sdb $ > /dev/null; then
+if ! blkid /dev/sdb &>/dev/null; then
   mkfs.xfs /dev/sdb
 fi
 
 mkdir -p /var/mnt/data
 data_uuid=$(blkid -s UUID -o value /dev/sdb)
-grep -q "$data_uuid" /etc/fstab || echo "UUID=${data_uuid} /var/mnt/data xfs defaults 0 2" >> /etc/fstab
+grep -q "$data_uuid" /etc/fstab || echo "UUID=$${data_uuid} /var/mnt/data xfs defaults 0 2" >> /etc/fstab
 mount -a
 
 # install tailscale
